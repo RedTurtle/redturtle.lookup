a88f629adae67a334c2b9c854c69ba5b42d04a78
mamico <mauro.amico@gmail.com>
2010-11-15 14:50:24 +0000

    zelenium tests

--- redturtle/lookup/browser.py
+++ redturtle/lookup/browser.py
@@ -1,8 +1,13 @@
 from zope.interface import implements
-from Acquisition import aq_base
+# from Acquisition import aq_base
 from Products.Five.browser import BrowserView
 from interfaces import ILookup
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+try:
+    from Products.Zelenium.interfaces import IZuite
+    ZELENIUM_INSTALLED = True
+except:
+    ZELENIUM_INSTALLED = False
 
 
 class Lookup(BrowserView):
@@ -18,3 +23,10 @@ class Lookup(BrowserView):
         for obj in self.context.values():
             if IPloneSiteRoot.providedBy(obj):
                 yield obj
+
+    def zelenium_suites(self):
+        if ZELENIUM_INSTALLED:
+            for obj in self.context.values():
+                if IZuite.providedBy(obj):
+                    yield obj
+
